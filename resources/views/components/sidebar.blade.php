457e79f9a56dfe7b0e46e3671e9298b132dfd287
kingmikomv <kingmikomv@gmail.com>
2025-08-05 22:06:46 +0700

    Update Baru

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,46 +1,41 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="https://png.pngtree.com/png-vector/20211106/ourmid/pngtree-letter-m-logo-png-image_4008793.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-            style="opacity: .8">
+        <img src="https://png.pngtree.com/png-vector/20211106/ourmid/pngtree-letter-m-logo-png-image_4008793.png"
+            alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">MANAGE DB</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
+        <!-- Sidebar user panel -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="https://cdn-icons-png.freepik.com/512/1794/1794749.png" class="img-circle elevation-2" alt="User Image">
+                <img src="https://cdn-icons-png.freepik.com/512/1794/1794749.png" class="img-circle elevation-2"
+                    alt="User Image">
             </div>
             <div class="info">
                 <a href="#" class="d-block">{{ Auth()->user()->name }}</a>
             </div>
         </div>
 
-        <!-- SidebarSearch Form -->
-
-
         <!-- Sidebar Menu -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                data-accordion="false">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
+                <!-- Static Link -->
                 <li class="nav-item">
                     <a href="{{ route('vpn.index') }}" class="nav-link">
                         <i class="nav-icon fas fa-th"></i>
-                        <p>
-                            Network
-                            <span class="right badge badge-danger">New</span>
-                        </p>
+                        <p>Network <span class="right badge badge-danger">New</span></p>
                     </a>
                 </li>
 
-
-
+                <!-- Section Header -->
                 <li class="nav-header">TOOLS</li>
 
-                <li class="nav-item">
+                <!-- MikroTik Treeview -->
+                <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-book"></i>
                         <p>
@@ -54,8 +49,7 @@
                                 <a href="{{ route('masukmikrotik', [
                                     'ipmikrotik' => $listmikrotik->ipmikrotik,
                                     'portweb' => $listmikrotik->portweb,
-                                ]) }}"
-                                    class="nav-link">
+                                ]) }}" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>{{ $listmikrotik->site }}</p>
                                 </a>
@@ -63,8 +57,9 @@
                         @endforeach
                     </ul>
                 </li>
-              
-                <li class="nav-item">
+
+                <!-- OLT Treeview -->
+                <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-clone"></i>
                         <p>
@@ -75,8 +70,7 @@
                     <ul class="nav nav-treeview">
                         @foreach ($olt as $listolt)
                             <li class="nav-item">
-                                <a href="{{"http://akses.aqtnetwork.my.id:". $listolt->portvpn}}" target="_blank"
-                                    class="nav-link">
+                                <a href="{{ 'http://akses.aqtnetwork.my.id:' . $listolt->portvpn }}" target="_blank" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>{{ $listolt->site }}</p>
                                 </a>
@@ -84,19 +78,17 @@
                         @endforeach
                     </ul>
                 </li>
-                  <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                data-accordion="false">
 
-               <li class="nav-item">
-    <a href="#" class="nav-link" onclick="confirmLogout(event)">
-        <i class="nav-icon fas fa-door-open"></i>
-        <p>Logout</p>
-    </a>
-
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-        @csrf
-    </form>
-</li>
+                <!-- Logout -->
+                <li class="nav-item">
+                    <a href="#" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="nav-icon fas fa-door-open"></i>
+                        <p>Logout</p>
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </li>
 
             </ul>
         </nav>
